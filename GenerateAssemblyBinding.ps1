f1495e340be672e5e9f4f9c6196cfb1fde751765
bgyu <bgyu@users.noreply.github.com>
2023-03-31 22:41:06 +1100

    Update GenerateAssemblyBinding.ps1

--- GenerateAssemblyBinding.ps1
+++ GenerateAssemblyBinding.ps1
@@ -22,21 +22,32 @@ $bindingInfo = @()
 # Loop through each assembly and generate binding information
 foreach ($assembly in $assemblies) {
     $assemblyName = [Reflection.AssemblyName]::GetAssemblyName($assembly.FullName)
+    Write-Host "Processing $assemblyName"
     $name = $assemblyName.Name
     $version = $assemblyName.Version
     $publicKeyToken = [BitConverter]::ToString($assemblyName.GetPublicKeyToken()).Replace("-","").ToLower()
+
+    if (!$PublicKeyToken) {
+        Write-Host "Skipping $AssemblyName, because it is not strongly named."
+        continue
+    }
+
     $culture = $assemblyName.CultureName
-    
-    $bindingInfo += "<dependentAssembly>"
-    $bindingInfo += "    <assemblyIdentity name=`"$name`" publicKeyToken=`"$publicKeyToken`" culture=`"$culture`" />"
-    $bindingInfo += "    <bindingRedirect oldVersion=`"0.0.0.0-$version`" newVersion=`"$version`" />"
-    $bindingInfo += "</dependentAssembly>"
+    $bindingInfo += "      <dependentAssembly>"
+    $bindingInfo += "        <assemblyIdentity name=`"$name`" publicKeyToken=`"$publicKeyToken`" culture=`"$culture`" />"
+    $bindingInfo += "        <bindingRedirect oldVersion=`"0.0.0.0-$version`" newVersion=`"$version`" />"
+    $bindingInfo += "      </dependentAssembly>"
 }
 
 # Generate the app.config file with the binding information
-$config = "<?xml version=`"1.0`" encoding=`"utf-8`" ?><configuration><runtime><assemblyBinding xmlns=`"urn:schemas-microsoft-com:asm.v1`">"
+$config = "<?xml version=`"1.0`" encoding=`"utf-8`" ?>`n"
+$config += "<configuration>`n"
+$config += "  <runtime>`n"
+$config += "    <assemblyBinding xmlns=`"urn:schemas-microsoft-com:asm.v1`">`n"
 $config += $bindingInfo -join "`n"
-$config += "</assemblyBinding></runtime></configuration>"
+$config += "`n    </assemblyBinding>`n"
+$config += "  </runtime>`n"
+$config += "</configuration>"
 
 # Write the app.config file to disk
 if ($BindingOutputPath) {
@@ -47,8 +58,8 @@ if ($BindingOutputPath) {
 
 Set-Content -Path $configPath -Value $config
 
-# Use assembly binding script in MSBuild target
-# <Target Name="GenerateAssemblyBindings" AfterTargets="AfterBuild">
-#   <Exec Command="powershell.exe -File GenerateAssemblyBindings.ps1 -FolderPath $(OutputPath) -OutputPath $(OutputPath)app.config" />
-# </Target>
 
+# Use in MSBuild AfterBuild target to generate app.config automatically
+# <Target Name="GenerateAssemblyBindings" AfterTargets="AfterBuild">
+#    <Exec Command="powershell.exe -File GenerateAssemblyBindings.ps1 -FolderPath $(OutputPath) -BindingOutputPath $(OutputPath)\app.config" />
+# </Target>
