6e93e52c6c4782b4815e7bbea5a4873eee39c31f
bgyu <bgyu@users.noreply.github.com>
2023-06-27 20:47:59 +1000

    Update CheckVersion.cmake

--- CheckVersion.cmake
+++ CheckVersion.cmake
@@ -4,16 +4,15 @@ function(Check_Version VERSION_NAME)
             message(FATAL_ERROR "${VERSION_NAME} is not provided!")
         else()
             set(${VERSION_NAME} $ENV{${VERSION_NAME}})
-            message(STATUS "${VERSION_NAME}=${${VERSION_NAME}}")
         endif()
-    else()
-        message(STATUS "${VERSION_NAME}=${${VERSION_NAME}}")
     endif()
 
     # Check if the version is empty or consists of only whitespace characters
     string(STRIP ${VERSION_NAME} ${VERSION_NAME})
     if("${${VERSION_NAME}}" STREQUAL "")
         message(FATAL_ERROR "${VERSION_NAME} is empty or consists of only whitespace characters!")
+    else()
+        message(STATUS "${VERSION_NAME}=${${VERSION_NAME}}")
     endif()
 endfunction()
 
